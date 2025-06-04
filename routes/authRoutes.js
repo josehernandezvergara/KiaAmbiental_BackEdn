d7d226ff8307618c4cb712b1cdf7854290d79b7f
Ayax <ayaxgaytan@gmail.com>
2025-06-04 16:11:59 -0600

    usuario empleado

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const verificarToken = require('../middleware/verificarToken');
+const esAdmin = require('../middleware/esAdmin');
 
 
 //al consumirla ingresaremos la informacion del usuario para obtener el token
 router.post('/login', authController.login);
 //para registrar un nuevo usuario
-router.post('/registro', authController.crearUsuario);
+router.post('/registro', authController.crearUsuario, esAdmin, verificarToken);
 //para obtener el perfil (informacion dle usuario) ruta protegida 
 router.get('/perfil', verificarToken, authController.perfil);
 
